dca077e7bd76cd6196e1fd288574a2bd7dcceeb0
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-12-26 22:51:41 +0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/' />
         <Route path='/cart' />
